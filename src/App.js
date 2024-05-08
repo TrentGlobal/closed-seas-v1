0729a50fed17d637522d9ac003d6358b27698da6
John Benedict Yan <johnbenedictyan@gmail.com>
2024-05-08 18:14:13 +0800

    update to version 2

--- src/App.js
+++ src/App.js
@@ -1,19 +1,37 @@
+import { useState } from "react";
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Marketplace from "./components/Marketplace";
+import Navbar from "./components/Navbar";
 import NFTPage from "./components/NFTpage";
 import Profile from "./components/Profile";
 import SellNFT from "./components/SellNFT";
 
 function App() {
+  const [loading, setLoading] = useState(false);
+
+  console.log(typeof setLoading)
+  console.log(setLoading)
+
   return (
-    <div className="container">
-      <Routes>
-        <Route path="/" element={<Marketplace />} />
-        <Route path="/nftPage/:tokenId" element={<NFTPage />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/sellNFT" element={<SellNFT />} />
-      </Routes>
+    <div className="w-screen flex justify-center">
+      <div className="container">
+        <header>
+          <Navbar loading={loading} setLoading={setLoading} />
+        </header>
+        <main>
+          {loading ? (
+            "Loading ..."
+          ) : (
+            <Routes>
+              <Route path="/" element={<Marketplace />} />
+              <Route path="/nftPage/:tokenId" element={<NFTPage />} />
+              <Route path="/profile" element={<Profile />} />
+              <Route path="/sellNFT" element={<SellNFT />} />
+            </Routes>
+          )}
+        </main>
+      </div>
     </div>
   );
 }
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,81 +1,43 @@
-import axios from "axios";
-import { useState } from "react";
-import { useParams } from "react-router-dom";
-import MarketplaceJSON from "../Marketplace.json";
+import { useEffect, useState } from "react";
+import { getNFTData } from "../services/nftService";
 import NFTTile from "./NFTTile";
-import Navbar from "./Navbar";
+import Spinner from "./Spinner";
 
 export default function Profile() {
   const [data, updateData] = useState([]);
-  const [dataFetched, updateFetched] = useState(false);
   const [address, updateAddress] = useState("0x");
   const [totalPrice, updateTotalPrice] = useState("0");
+  const [loading, setLoading] = useState(false);
 
-  async function getNFTData(tokenId) {
-    const ethers = require("ethers");
-    let sumPrice = 0;
-    //After adding your Hardhat network to your metamask, this code will get providers and signers
-    const provider = new ethers.providers.Web3Provider(window.ethereum);
-    const signer = provider.getSigner();
-    const addr = await signer.getAddress();
-
-    //Pull the deployed contract instance
-    let contract = new ethers.Contract(
-      MarketplaceJSON.address,
-      MarketplaceJSON.abi,
-      signer
-    );
-
-    //create an NFT Token
-    let transaction = await contract.getMyNFTs();
-
-    /*
-     * Below function takes the metadata from tokenURI and the data returned by getMyNFTs() contract function
-     * and creates an object of information that is to be displayed
-     */
-
-    const items = await Promise.all(
-      transaction.map(async (i) => {
-        const tokenURI = await contract.tokenURI(i.tokenId);
-        let meta = await axios.get(tokenURI);
-        meta = meta.data;
-
-        let price = ethers.utils.formatUnits(i.price.toString(), "ether");
-        let item = {
-          price,
-          tokenId: i.tokenId.toNumber(),
-          seller: i.seller,
-          owner: i.owner,
-          image: meta.image,
-          name: meta.name,
-          description: meta.description,
-        };
-        sumPrice += Number(price);
-        return item;
+  useEffect(() => {
+    setLoading(true);
+    getNFTData()
+      .then((res) => {
+        const { items, addr, sumPrice } = res;
+        updateData(items);
+        updateAddress(addr);
+        updateTotalPrice(sumPrice);
       })
-    );
+      .catch((err) => {
+        console.log(err);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
 
-    updateData(items);
-    updateFetched(true);
-    updateAddress(addr);
-    updateTotalPrice(sumPrice.toPrecision(3));
-  }
-
-  const params = useParams();
-  const tokenId = params.tokenId;
-  if (!dataFetched) getNFTData(tokenId);
+  if (loading) return <Spinner />;
 
   return (
     <div className="profileClass" style={{ minHeight: "100vh" }}>
-      <Navbar></Navbar>
       <div className="profileClass">
-        <div className="flex text-center flex-col mt-11 md:text-2xl text-white">
+        <div className="flex text-center flex-col mt-11 md:text-2xl text-dark">
           <div className="mb-5">
             <h2 className="font-bold">Wallet Address</h2>
             {address}
           </div>
         </div>
-        <div className="flex flex-row text-center justify-center mt-10 md:text-2xl text-white">
+        <div className="flex flex-row text-center justify-center mt-10 md:text-2xl text-dark">
           <div>
             <h2 className="font-bold">No. of NFTs</h2>
             {data.length}
@@ -85,18 +47,19 @@ export default function Profile() {
             {totalPrice} ETH
           </div>
         </div>
-        <div className="flex flex-col text-center items-center mt-11 text-white">
-          <h2 className="font-bold">Your NFTs</h2>
-          <div className="flex justify-center flex-wrap max-w-screen-xl">
-            {data.map((value, index) => {
-              return <NFTTile data={value} key={index}></NFTTile>;
-            })}
-          </div>
-          <div className="mt-10 text-xl">
-            {data.length == 0
-              ? "Oops, No NFT data to display (Are you logged in?)"
-              : ""}
-          </div>
+        <h2 className="text-2xl font-bold tracking-tight text-gray-900">
+          Your NFTs
+        </h2>
+
+        <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+          {data.map((value, index) => (
+            <NFTTile data={value} key={index} />
+          ))}
+        </div>
+        <div className="mt-10 text-xl">
+          {data.length == 0
+            ? "Oops, No NFT data to display (Are you logged in?)"
+            : ""}
         </div>
       </div>
     </div>
